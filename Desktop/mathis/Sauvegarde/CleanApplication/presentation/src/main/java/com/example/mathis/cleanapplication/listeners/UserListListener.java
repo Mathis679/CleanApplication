eeccf60f6e9a7625edecf7d9ed2ce9afaf06b5bd
Mathis679 <mathis.laurent.pro@gmail.com>
2017-05-19 11:10:49 +0200

    add listener interface

---
+++ Desktop/mathis/Sauvegarde/CleanApplication/presentation/src/main/java/com/example/mathis/cleanapplication/listeners/UserListListener.java
@@ -0,0 +1,11 @@
+package com.example.mathis.cleanapplication.listeners;
+
+import android.view.View;
+
+/**
+ * Created by e-Conception on 19/05/2017.
+ */
+
+public interface UserListListener extends View.OnClickListener {
+    void onAddUser();
+}
